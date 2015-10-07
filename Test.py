d44c3bbac20f969fa77a683885f6e54d289a69d9
Samuel Lipschutz <lipschut@pike.nscl.msu.edu>
2015-10-07 10:51:59 -0400

    Adding test

---
+++ Test.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python2.7
+
+import sys
+from PyQt4 import uic, QtGui, QtCore
+from PyQt4.QtGui import *
+from PyQt4.QtCore import *
+import LoadDeductionDataBase
+import LoadCBUDataBase
+import LoadReconciledDataBase
+
+import DeductionLoader
+import CBULoader
+
+from os.path import join
+
+import matplotlib.pyplot as plt
+
+from Reconcile import Reconcile
+
+
+
+
+if __name__=='__main__':
+    print "I know how to program"
+
+    fileName1="./DeductionData/20150904-GEUDues.xlsx"
+    fileName2="./DeductionData/20150918-GEUDues.xlsx"
+
+
+
+
+    NormalMap1,LastFirstNameMap=DeductionLoader.LoadDeduction(fileName1);
+    NormalMap2,LastFirstNameMap=DeductionLoader.LoadDeduction(fileName2);
+
+    CBUMap,trash,trash1 = CBULoader.LoadCBU("./CBUData/20150901-GEUCBU.xlsx")
+
+
+    print len(NormalMap1)
+    print len(NormalMap2)
+    
+
+    
+    listOfPeople={}
+    for netid,info in NormalMap1.iteritems():
+        if netid in NormalMap2:
+            pass
+        else:
+            if info.EmployeeGroup=="Union":
+                listOfPeople[netid]=info
+
+
+    for netid,info in listOfPeople.iteritems():
+        if netid in CBUMap:
+            print netid," ",info.FirstName," ",info.LastName," ",info.WageTypeText
+        else:
+            pass
+    
+
+
+
+
+
+
+
+
+
+
+
+
