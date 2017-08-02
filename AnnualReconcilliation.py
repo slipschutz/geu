17b8810e3ae264f613e4cbbbf3add3e0ef72c93b
Sam Lipschutz <samlipschutz@Sams-MacBook-Pro.local>
2017-08-01 22:29:58 -0400

    Made the AnnualReconcilliation more modular so that it will be easier to do the Dues and the Fees reconcilliation

--- AnnualReconcilliation.py
+++ AnnualReconcilliation.py
@@ -22,6 +22,90 @@ class PerCapParameters:
 
 
 
+def ProcessSalaryList(SalaryTotals,TheParams,MapForAllDeductionFiles,theName):
+
+    FullCutOff=TheParams.FullTime_CutOff
+    HalfCutOff=TheParams.HalfTime_CutOff
+    QuarterCutOff=TheParams.QuarterTime_CutOff
+    EighthCutOff=TheParams.EighthTime_CutOff
+
+
+    FullRateAFT=getattr(TheParams,"FullTime_AFT{0}PerCap".format(theName))
+    HalfRateAFT=getattr(TheParams,"HalfTime_AFT{0}PerCap".format(theName))
+    QuarterRateAFT=getattr(TheParams,"QuarterTime_AFT{0}PerCap".format(theName))
+    EighthRateAFT=getattr(TheParams,"EighthTime_AFT{0}PerCap".format(theName))
+
+
+    FullRateAFTMI=getattr(TheParams,"FullTime_AFTMI{0}PerCap".format(theName))
+    HalfRateAFTMI=getattr(TheParams,"HalfTime_AFTMI{0}PerCap".format(theName))
+    QuarterRateAFTMI=getattr(TheParams,"QuarterTime_AFTMI{0}PerCap".format(theName))
+    EighthRateAFTMI=getattr(TheParams,"EighthTime_AFTMI{0}PerCap".format(theName))
+
+    
+    
+    FullTimeMap={}
+    HalfTimeMap={}
+    QuarterTimeMap={}
+    EighthTimeMap={}
+    
+    for netid,Total in SalaryTotals.items():
+        if Total >= FullCutOff:
+            FullTimeMap[netid]=Total
+        elif Total >= HalfCutOff:
+            HalfTimeMap[netid]=Total
+        elif Total >= QuarterCutOff:
+            QuarterTimeMap[netid]=Total
+        elif Total >=EighthCutOff:
+            EighthTimeMap[netid]=Total
+
+
+
+
+    monthChecker={}
+
+    orderedDateList=[]
+    for date,DedWrap in MapForAllDeductionFiles.items():
+        orderedDateList.append(date)
+
+    orderedDateList= sorted(orderedDateList)
+
+    total=0
+    for date in orderedDateList:
+        DedWrap=MapForAllDeductionFiles[date]
+        NumFull=0
+        NumHalf=0
+        NumQuarter=0
+        NumEighth=0
+
+        for netid,info in DedWrap.NetIdMap.items():
+            if netid in FullTimeMap:
+                NumFull=NumFull+1
+            elif netid in HalfTimeMap:
+                NumHalf=NumHalf+1
+            elif netid in QuarterTimeMap:
+                NumQuarter=NumQuarter+1
+            elif netid in EighthTimeMap:
+                NumEighth=NumEighth+1
+        
+        month=date.month 
+        if month not in monthChecker:
+            monthChecker[month]=1
+        elif monthChecker[month]==1:
+            monthChecker[month]=2
+            totalNumberOfPeople=(NumFull +NumHalf+NumQuarter+NumEighth)
+            Orig = totalNumberOfPeople*QuarterRateAFT
+            New = NumFull*FullRateAFT+  NumHalf*HalfRateAFT +   NumQuarter*QuarterRateAFT +   NumEighth*EighthRateAFT
+            OrigMI = totalNumberOfPeople*QuarterRateAFTMI
+            NewMI = NumFull*FullRateAFTMI+  NumHalf*HalfRateAFTMI +   NumQuarter*QuarterRateAFTMI +   NumEighth*EighthRateAFTMI
+
+            print(date,"Full ",NumFull,"Half",NumHalf,"Quarter",NumQuarter,"Eighth",NumEighth,"totPeps",totalNumberOfPeople,"Orig {0:5.2f}".format(Orig)," new {0:5.2f}".format(New),"OrigMI {0:5.2f}".format(OrigMI),"new MI {0:5.2f}".format(NewMI))
+
+
+            total=total+(New-Orig) +(NewMI-OrigMI)
+
+    print (total)
+    
+
 def DoAnnualReconcilliation(deductionFileNames,TheParams):
     # PayLevels=["FullTime","HalfTime","QuarterTime","EighthTime"]
     # Things=["CutOff","AFTMemPerCap","AFTFeePerCap","AFTMIMemPerCap","AFTMIFeePerCap"]
@@ -62,6 +146,18 @@ def DoAnnualReconcilliation(deductionFileNames,TheParams):
                     SalaryTotals[netid]=tempDeduction/0.016
 
     print("Size of Salary totals", len(SalaryTotals))
+
+    ProcessSalaryList(SalaryTotals,TheParams,MapForAllDeductionFiles,"Mem")
+    return
+
+
+
+
+
+
+
+
+
 
     FullCutOff=TheParams.FullTime_CutOff
     HalfCutOff=TheParams.HalfTime_CutOff
@@ -125,8 +221,9 @@ def DoAnnualReconcilliation(deductionFileNames,TheParams):
             Orig = (NumFull +NumHalf+NumQuarter+NumEighth)*QuarterDuesAFT
             New = NumFull*FullDuesAFT+NumHalf*HalfDuesAFT + NumQuarter*QuarterDuesAFT + NumEighth*EighthDuesAFT
             print(date,"Full ",NumFull,"Half",NumHalf,"Quarter",NumQuarter,"Eighth",NumEighth,"Orig {0:5.2f}".format(Orig)," new {0:5.2f}".format(New))
-            total=total+(new-Orig)
+            total=total+(New-Orig)
 
+    print (total)
     
 if __name__=='__main__':
     Test()
